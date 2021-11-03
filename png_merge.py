875e65018c9dafcf15bd360f69247f6606075303
sheerluck <sheer.luck.andrew@gmail.com>
2021-11-03 09:20:06 +0300

    reversal = True for low-quality updates to use them as background
    Turns out adding a new argument lures them all into dictionary
    
    Basically we flip alpha_composite(bi, fi) to alpha_composite(fi, bi)
    
    closes #2

--- png_merge.py
+++ png_merge.py
@@ -2,7 +2,7 @@ import os
 import click
 import contextlib
 import pathlib
-from typing import Tuple
+from typing import Any, Tuple
 from PIL import Image
 from colorama import Fore, init as colorama_init
 from shutil import copyfile
@@ -12,7 +12,8 @@ def deslash(s: str) -> str:
     return s.replace("//", "/")
 
 
-def both(base: str, u: str, u11: str, fn: str) -> Tuple[str, str]:
+def both(conf: dict, u11: str, fn: str) -> Tuple[str, str]:
+    base, u = conf["prod"], conf["update"]
     if u11.startswith(u):
         info = u11.removeprefix(u)
         bg = f"{base}/{info}/{fn}"
@@ -37,21 +38,24 @@ def check_errors(bi: Image, fi: Image) -> None:
         raise ValueError(msg)
 
 
-def composite(bg: str, fg: str) -> None:
+def composite(bg: str, fg: str, flip: bool) -> None:
     bi = Image.open(bg)
     fi = Image.open(fg)
     check_errors(bi, fi)
     print(Fore.CYAN + fg, end=" ")
     print(f"{fi.format}, {fi.mode}, {fi.size}")
-    Image.alpha_composite(bi, fi).save(bg)
+    if flip:
+        Image.alpha_composite(fi, bi).save(bg)
+    else:
+        Image.alpha_composite(bi, fi).save(bg)
 
 
-def process(prod: str, updates: str, upath: str, files: list) -> None:
+def process(conf: dict, upath: str, files: list) -> None:
     for fn in sorted(files):
         if fn.endswith(".png"):
-            bg, fg = both(prod, updates, upath, fn)
+            bg, fg = both(conf, upath, fn)
             if pathlib.Path(bg).is_file():
-                composite(bg, fg)
+                composite(bg, fg, conf["flip"])
             else:
                 print(Fore.RED + f"no base {bg} for update {fg}, coping")
                 p = pathlib.Path(bg).parent
@@ -60,19 +64,25 @@ def process(prod: str, updates: str, upath: str, files: list) -> None:
 
 
 @click.command()
-@click.option('--test',    default="False",                         help='try --test=True')
-@click.option('--prod',    default="/media/heights/public/256",     help='path to base')
-@click.option('--updates', default="/media/heights/public/updates", help='path to updates')
-def main(test: str,
+@click.option('--test',     default=False,             help='try --test=True')
+@click.option('--prod',     default="/public/256",     help='path to base')
+@click.option('--updates',  default="/public/updates", help='path to updates')
+@click.option('--reversal', default=False, help='For low-quality updates ' +
+                                                'to use them as background')
+def main(test: bool,
          prod: str,
-         updates: str) -> int:
-    """First line should be in imperative mood; try rephrasing."""
+         updates: str,
+         reversal: bool) -> int:
+    """Merge png recursively using alpha_composite"""
 
-    if test == "True":
+    conf: dict[str, Any] = {"prod": prod, "update": updates, "flip": reversal}
+
+    if test:
         with contextlib.suppress(ValueError):
-            bg, fg = both("/mnt/256/", "a", "b", "100.png")
-        bg, fg = both("/mnt/256/", "/mnt/updates/",
-                                   "/mnt/updates/8/45/", "100.png")
+            conf = {"prod": "/mnt/256/", "update": "a"}
+            bg, fg = both(conf, "b", "100.png")
+        conf = {"prod": "/mnt/256/", "update": "/mnt/updates/"}
+        bg, fg = both(conf,                    "/mnt/updates/8/45/", "100.png")
         print(f"{bg=}")
         print(f"{fg=}")
         return 0
@@ -80,7 +90,7 @@ def main(test: str,
     colorama_init(autoreset=True)
     for a, _, u in os.walk(updates):
         if u:
-            process(prod, updates, a, u)
+            process(conf, a, u)
 
     return 0
 
